e5c5fbb5e8063c67c14d61fe4de56b94ebca8eff
chris <chrishethertonweb@gmail.com>
2022-10-12 14:47:25 +0200

    no idea

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import './App.css'
 import Envelope from "./Envelope";
-import AddressLabel from './AddressLabel'
 
 export default function App() {
     return (
-        <div className="border border-dark" >
-            <h1>My React App</h1>
+        <div >
+            <Envelope />
         </div>
     )
 }
