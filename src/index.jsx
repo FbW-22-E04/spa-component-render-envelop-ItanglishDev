27c9dece8574db55bc0d9d7ccabb5c30bedcf35e
chris <chrishethertonweb@gmail.com>
2022-10-12 13:24:08 +0200

    added Envelope.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Envelope from "./Envelope";
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById("root"));
